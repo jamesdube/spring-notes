f62b9cee69f54bf02d7d165d56702a15a9e481e7
James Dube <jamesdylandube@gmail.com>
2018-10-10 11:09:12 +0200

    initial project structure

---
+++ src/main/java/com/jamesdube/springnotes/SpringNotesApplication.java
@@ -0,0 +1,12 @@
+package com.jamesdube.springnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNotesApplication.class, args);
+	}
+}
